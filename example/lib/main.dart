6b68f48ec57504052501a9bd3ca480d33cc74644
Surafel Melese <surafelmelese09@gmail.com>
2022-03-16 23:15:02 +0300

    added a working example

---
+++ example/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:example/set_alarm.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SetAlarm(),
+    );
+  }
+}

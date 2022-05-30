7b102443408a02f90d6127f70ad33c14ff16b26e
Martin Berthier <martin.berthier.mb@gmail.com>
2022-05-30 15:32:02 +0200

    commenting + removing unused code

--- lib/time_duration_picker.dart
+++ lib/time_duration_picker.dart
@@ -7,32 +7,55 @@ import 'Clock/clock.dart';
 import 'Knob/knob.dart';
 
 class TimeDurationPicker extends StatelessWidget {
+  /// Knob's first icon
   final IconData icon1Data;
+
+  /// Knob's second icon
   final IconData icon2Data;
+
+  /// Widget's total radius
   final double radius;
+
+  /// Knob icon's colors
   final Color iconColor;
+
+  /// Knob's color
   final BoxDecoration? knobDecoration;
+
+  /// Knob's background
   final BoxDecoration? knobBackgroundDecoration;
+
+  /// Clock container's box decoration
   final BoxDecoration? clockDecoration;
+
+  /// Clock number's text style
   final TextStyle? clockTextStyle;
+
+  /// Set this to true to have a clock using AM-PM
   final bool twelveHourClock;
-  final void Function(SimpleTime) onIcon1RotatedCallback;
-  final void Function(SimpleTime) onIcon2RotatedCallback;
-  final void Function(SimpleTime)? setDurationCallback;
+
+  /// Called back when the first icon is moved
+  final void Function(SimpleTime) onIcon1Move;
+
+  /// Called back when the second icon is moved
+  final void Function(SimpleTime) onIcon2Move;
+
+  /// Call back whenever an icon is moved
+  final void Function(SimpleTime)? onIconMove;
 
   const TimeDurationPicker({
     Key? key,
     required this.icon1Data,
     required this.icon2Data,
     required this.radius,
-    required this.onIcon1RotatedCallback,
-    required this.onIcon2RotatedCallback,
+    required this.onIcon1Move,
+    required this.onIcon2Move,
     this.iconColor = Colors.white,
     this.knobDecoration,
     this.knobBackgroundDecoration,
     this.clockDecoration,
     this.clockTextStyle,
-    this.setDurationCallback,
+    this.onIconMove,
     this.twelveHourClock = true,
 
   }) : super(key: key);
@@ -60,24 +83,19 @@ class TimeDurationPicker extends StatelessWidget {
                   radius: radius,
                   innerRadiusRatio: knobInnerRadiusRatio,
                   outerRadiusRatio: knobOuterRadiusRatio,
-                  onIcon1RotatedCallback: onIcon1RotatedCallback,
-                  onIcon2RotatedCallback: onIcon2RotatedCallback,
+                  onIcon1RotatedCallback: onIcon1Move,
+                  onIcon2RotatedCallback: onIcon2Move,
                   icon1Data: icon1Data,
                   icon2Data: icon2Data,
                   iconColor: iconColor,
                   knobDecoration: knobDecoration,
-                  setDurationCallback: setDurationCallback,
+                  setDurationCallback: onIconMove,
                 ),
-                /*Padding(
-                  padding: EdgeInsets.all(radius * gapRatio),
-                  child: Center(
-                    child: */Clock(
-                      twelveHourClock: twelveHourClock,
-                      radius: radius * clockRatio,
-                      clockDecoration: clockDecoration,
-                      clockTextStyle: clockTextStyle,
-                    //),
-                  //),
+                Clock(
+                  twelveHourClock: twelveHourClock,
+                  radius: radius * clockRatio,
+                  clockDecoration: clockDecoration,
+                  clockTextStyle: clockTextStyle,
                 ),
               ],
             ),
